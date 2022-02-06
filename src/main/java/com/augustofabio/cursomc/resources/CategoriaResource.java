44892c4d44cd588ce4c67c336bf8c9fed00510b2
Binho1908 <fabiosenninhaddt@gmail.com>
2022-02-06 00:25:07 -0300

    testando a primeira classe de dominio

--- src/main/java/com/augustofabio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/augustofabio/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.augustofabio.cursomc.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.augustofabio.cursomc.domain.Categoria;
 
 
 
@@ -10,7 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET) // associando com um metodo http
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
